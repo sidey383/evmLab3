13e12c0b3cdcf6bb569ca70ee05a0fb7bdb53a5c
sidey383 <sidey383@mail.ru>
2022-11-14 22:36:20 +0700

    add timers, merge with other project

--- camera.cpp
+++ camera.cpp
@@ -5,8 +5,25 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 
+#if defined __linux__
+#include <time.h>
+
+unsigned long long getNanoTime() {
+    timespec time;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+    return time.tv_sec * 1000000000 + time.tv_nsec;
+}
+#endif
+#if defined _WIN32
+#include <Windows.h>
+
+unsigned long long getNanoTime() {
+    return GetTickCount64()*1000000;
+}
+
+#endif
+
 using namespace cv;
-using namespace std;
 
 void bigPixels(Mat frame, int width, int height, int pixelSize) {
     for (int x = 0; x < width/pixelSize + 1; x++) {
@@ -41,7 +58,9 @@ int main() {
         try
         {
             VideoCapture camera(numberOfDevices);
-            std::cout<<camera.getBackendName()<<" WIDTH:"<<camera.get(CV_CAP_PROP_FRAME_WIDTH)<<" HEIGHT:"<<camera.get(CV_CAP_PROP_FRAME_HEIGHT)<<"\n";
+            std::cout << camera.getBackendName()
+                << " WIDTH:" << camera.get(CV_CAP_PROP_FRAME_WIDTH)
+                << " HEIGHT:" << camera.get(CV_CAP_PROP_FRAME_HEIGHT) << std::endl  ;
             numberOfDevices++;
             cam = camera;
         }
@@ -51,20 +70,45 @@ int main() {
         }
     }
 
-    cout<<"count: "<<numberOfDevices<<"\n";
-
+    if(numberOfDevices == 0) {
+        std::cout << "no camera";
+        return 0;
+    } else {
+        std::cout << "count: " << numberOfDevices << std::endl;
+    }
 
     Mat frame;
     int width = cam.get(CV_CAP_PROP_FRAME_WIDTH);
     int height = cam.get(CV_CAP_PROP_FRAME_HEIGHT);
     cam.open(0);
-    while (1) {
+    std::cout << "start camera";
+    unsigned long long inputTime = 0;
+    unsigned long long calculateTime = 0;
+    unsigned long long showTime = 0;
+    unsigned long long frameCount = 0;
+    unsigned long long start = getNanoTime();
+    while(1) {
+        unsigned long long time1 = getNanoTime();
         cam >> frame;
+        inputTime += getNanoTime() - time1;
+        time1 = getNanoTime();
         bigPixels(frame, height, width, 15);
-        imshow("changed", frame);
+        calculateTime += getNanoTime() - time1;
+        time1 = getNanoTime();
+        imshow("cadr", frame);
+        showTime += getNanoTime() - time1;
         char c = cv::waitKey(33);
         if (c == 27) {
             break;
+        }
+        frameCount++;
+        if(frameCount % 100 == 0) {
+            unsigned long long time = getNanoTime() - start;
+            std::cout<< "input time: " << inputTime << std::endl
+                << "calculate time: " << calculateTime << std::endl
+                << "show time: " << showTime << std::endl
+                << "total time: " << time  << std::endl
+                << "fps: " << frameCount / (time/1000000000) << std::endl;
         }
     }
 }
--- libUSB.cpp
+++ libUSB.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <libusb-1.0/libusb.h>
+#include "libusb-1.0/libusb.h"
 
 using namespace std;
 
